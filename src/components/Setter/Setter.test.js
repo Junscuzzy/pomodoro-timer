c3ba2078c51e03f66f8581746855fe3f14024c4c
julien <juliencaron18@gmail.com>
2019-11-21 23:41:50 +0100

    add Setter tests

--- src/components/Setter/Setter.test.js
+++ src/components/Setter/Setter.test.js
@@ -3,32 +3,55 @@ import ReactDOM from "react-dom";
 import { shallow } from "../../enzyme";
 import Setter from "./Setter";
 
+const fnIncrementClick = jest.fn();
+const fnDecrementClick = jest.fn();
+const jsx = (
+  <Setter
+    id="test"
+    value={5}
+    increment={fnIncrementClick}
+    decrement={fnDecrementClick}
+  />
+);
+
 describe("<Setter />", () => {
-  it("Must contains a label with an id", () => {
-    const component = shallow(<Setter id="test" initialValue={5} />);
-    const label = component.find("#test-label");
-    expect(label).toHaveLength(1);
+  describe("Test the HTML structure", () => {
+    it("sidebar should render correctly", () => {
+      const component = shallow(jsx);
+      expect(component).toMatchSnapshot();
+    });
+
+    it("Must contains a label, two buttons and value with an id", () => {
+      const component = shallow(jsx);
+
+      const label = component.find("#test-label");
+      const increment = component.find("#test-increment");
+      const decrement = component.find("#test-decrement");
+      const value = component.find("#test-length");
+
+      expect(label).toHaveLength(1);
+      expect(increment).toHaveLength(1);
+      expect(decrement).toHaveLength(1);
+      expect(value).toHaveLength(1);
+    });
   });
 
-  it("Must contains a increment button with an id", () => {
-    const component = shallow(<Setter id="test" initialValue={5} />);
-    const increment = component.find("#test-increment");
-    expect(increment).toHaveLength(1);
+  it("Must increment on click button", () => {
+    const component = shallow(jsx);
+    component.find("#test-increment").simulate("click");
+    expect(fnIncrementClick).toHaveBeenCalled();
   });
 
-  it("Must contains a decrement button with an id", () => {
-    const component = shallow(<Setter id="test" initialValue={5} />);
-    const decrement = component.find("#test-decrement");
-    expect(decrement).toHaveLength(1);
+  it("Must decrement on click button", () => {
+    const component = shallow(jsx);
+    component.find("#test-decrement").simulate("click");
+    expect(fnDecrementClick).toHaveBeenCalled();
   });
 
-  it("Must contains a text with an id contains value", () => {
-    const component = shallow(<Setter id="test" initialValue={5} />);
+  it("Must contains a value", () => {
+    const component = shallow(jsx);
     const text = component.find("#test-length");
     expect(text).toHaveLength(1);
-
-    expect(text.text()).not.toBe(null);
-    expect(text.text()).not.toBe("");
-    expect(text.text()).not.toBe(undefined);
+    expect(text.text()).toBe("5");
   });
 });
