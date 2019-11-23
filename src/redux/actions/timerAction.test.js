266bf104ec9c0ce469869cdc2a19a5446c3db922
julien <juliencaron18@gmail.com>
2019-11-23 02:22:39 +0100

    Update actions tests

--- src/redux/actions/timerAction.test.js
+++ src/redux/actions/timerAction.test.js
@@ -5,7 +5,7 @@ import { SECOND } from "../../utils/api";
 
 const store = makeMockStore();
 
-describe("Redux Timer Actions", () => {
+describe("Redux Timer action", () => {
   beforeEach(() => {
     // Runs before each test in the suite
     store.clearActions();
@@ -13,54 +13,66 @@ describe("Redux Timer Actions", () => {
 
   describe("incrementBreak()", () => {
     it("Dispatches the correct actions", async () => {
-      const expectedAction = {
-        type: types.INCREMENT_BREAK,
-        breakLength: 6
-      };
+      const expectedAction = [
+        {
+          type: types.INCREMENT_BREAK,
+          breakLength: 6
+        },
+        { type: types.RESET_TIME }
+      ];
 
       store.dispatch(actions.incrementBreak(5));
-      await expect(store.getActions()).toContainEqual(expectedAction);
+      await expect(store.getActions()).toEqual(expectedAction);
     });
   });
 
   describe("decrementBreak()", () => {
     it("Dispatches the correct actions", async () => {
-      const expectedAction = {
-        type: types.DECREMENT_BREAK,
-        breakLength: 4
-      };
+      const expectedActions = [
+        {
+          type: types.DECREMENT_BREAK,
+          breakLength: 4
+        },
+        { type: types.RESET_TIME }
+      ];
 
       store.dispatch(actions.decrementBreak(5));
-      await expect(store.getActions()).toContainEqual(expectedAction);
+      await expect(store.getActions()).toEqual(expectedActions);
     });
   });
 
   describe("incrementSession()", () => {
     it("Dispatches the correct actions", async () => {
-      const expectedAction = {
-        type: types.INCREMENT_SESSION,
-        sessionLength: 6
-      };
+      const expectedActions = [
+        {
+          type: types.INCREMENT_SESSION,
+          sessionLength: 6
+        },
+        { type: types.RESET_TIME }
+      ];
 
       store.dispatch(actions.incrementSession(5));
-      await expect(store.getActions()).toContainEqual(expectedAction);
+      await expect(store.getActions()).toEqual(expectedActions);
     });
   });
 
   describe("decrementSession()", () => {
     it("Dispatches the correct actions", async () => {
-      const expectedAction = {
-        type: types.DECREMENT_SESSION,
-        sessionLength: 4
-      };
+      const expectedActions = [
+        {
+          type: types.DECREMENT_SESSION,
+          sessionLength: 4
+        },
+        { type: types.RESET_TIME }
+      ];
 
       store.dispatch(actions.decrementSession(5));
-      await expect(store.getActions()).toContainEqual(expectedAction);
+      await expect(store.getActions()).toEqual(expectedActions);
     });
   });
 
   describe("startStop()", () => {
-    it("Dispatches the correct actions", async () => {
+    it("Dispatches the correct action", async () => {
       store.dispatch(actions.startStop());
       await expect(store.getActions()).toContainEqual({
         type: types.START_STOP
@@ -68,17 +80,26 @@ describe("Redux Timer Actions", () => {
     });
   });
 
-  describe("reset()", () => {
-    it("Dispatches the correct actions", async () => {
-      store.dispatch(actions.reset());
+  describe("resetTime()", () => {
+    it("Dispatches the correct action", async () => {
+      store.dispatch(actions.resetTime());
       await expect(store.getActions()).toContainEqual({
-        type: types.RESET
+        type: types.RESET_TIME
+      });
+    });
+  });
+
+  describe("resetTimer()", () => {
+    it("Dispatches the correct action", async () => {
+      store.dispatch(actions.resetTimer());
+      await expect(store.getActions()).toContainEqual({
+        type: types.RESET_TIMER
       });
     });
   });
 
   describe("updateTime()", () => {
-    it("Dispatches the correct actions", async () => {
+    it("Dispatches the correct action if time is positive", async () => {
       store.dispatch(actions.updateTime(5 * SECOND));
       await expect(store.getActions()).toContainEqual({
         type: types.UPDATE_TIME,
@@ -86,21 +107,22 @@ describe("Redux Timer Actions", () => {
       });
     });
 
-    it("Not Dispatches negative time", async () => {
+    it("Dispatches the correct action if time is negative ", async () => {
       store.dispatch(actions.updateTime(0));
       await expect(store.getActions()).toContainEqual({
-        type: types.UPDATE_TIME,
-        time: 0
+        type: types.TOGGLE_TIMER_TYPE
       });
     });
   });
 
-  describe("setTimerType()", () => {
-    it("Dispatches the correct actions", async () => {
+  describe("toggleTimerType()", () => {
+    it("Dispatches the correct action", async () => {
+      const expectedActions = [
+        { type: types.TOGGLE_TIMER_TYPE },
+        { type: types.RESET_TIME }
+      ];
       store.dispatch(actions.toggleTimerType());
-      await expect(store.getActions()).toContainEqual({
-        type: types.TOGGLE_TIMER_TYPE
-      });
+      await expect(store.getActions()).toEqual(expectedActions);
     });
   });
 });
