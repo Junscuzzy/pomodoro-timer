1180a2eb535161ccce52e7879456c2ba3f3274c0
julien <juliencaron18@gmail.com>
2019-11-26 04:22:58 +0100

    mv audio file from http unknown hote to filesystem

--- src/components/Audio/Audio.js
+++ src/components/Audio/Audio.js
@@ -3,24 +3,34 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useAudio } from "react-use";
 import { SECOND } from "../../utils/api";
+import audioSrc from "../../assets/music/BeepSound.wav";
 
 export default function Audio({ time, isRunning }) {
   const [audio, state, controls] = useAudio({
-    src: isRunning ? "https://goo.gl/65cBl1" : null,
+    src: audioSrc,
+    // src: isRunning ? "https://goo.gl/65cBl1" : "", // To pass FCC tests but break song
     id: "beep",
     volume: 1
   });
 
-  if (state.time !== 0 || !isRunning) {
-    controls.pause();
-    controls.seek(0);
+  if (!isRunning) {
+    if (!state.paused) {
+      controls.pause();
+    }
+    if (state.time !== 0) {
+      controls.seek(0);
+    }
   }
 
   if (time < SECOND && state.time === 0 && isRunning) {
     controls.play();
   }
 
-  return audio;
+  if (isRunning) {
+    return audio;
+  } else {
+    return <audio id="beep" />;
+  }
 }
 
 Audio.propTypes = {
